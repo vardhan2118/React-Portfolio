b1f5d04c4f9f2833c2c05081fe78d81eac21df3e
Chitravardhan Reddy <cchitravardhan21@gmail.com>
2024-03-12 21:12:47 +0530

    Add files via upload

---
+++ src/components/LandingSection.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Avatar, Heading, VStack } from "@chakra-ui/react";
+import FullScreenSection from "./FullScreenSection";
+
+const greeting = "Hello, I am Vardhan";
+const bio1 = "A frontend developer";
+const bio2 = "specialised in React";
+
+const LandingSection = () => (
+  <FullScreenSection
+    justifyContent="center"
+    alignItems="center"
+    isDarkBackground
+    backgroundColor="#2A4365"
+  >
+    <VStack>
+      <Avatar size="2xl" name="Vardhan" src="../images/hero1.jpg" />
+      <p>{greeting}</p>
+      <Heading>{bio1}</Heading>
+      <Heading>{bio2}</Heading>
+    </VStack>
+  </FullScreenSection>
+);
+
+export default LandingSection;
